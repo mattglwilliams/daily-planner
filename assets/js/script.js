a2420f55117d244945e60c02744c6f4efeabf853
Matthew Williams <matthewwilliams@matts-mbp.home>
2021-11-19 16:59:11 +0000

    Added functions to check the time and change the input colour based on the time

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,3 +56,90 @@ $("#btn5").click(function() {
 
 $('#input5').val(localStorage.getItem("input5"));
 
+var currentTime = moment().format('H');
+
+function checkTime(){
+    if (currentTime > 9) {
+        $('#input9').addClass("past")
+    }
+    if (currentTime == 9) {
+        $('#input9').addClass("present")
+    }
+    if (currentTime < 9) {
+        $('#input9').addClass("future")
+    }
+    if (currentTime > 10) {
+        $('#input10').addClass("past")
+    }
+    if (currentTime == 10) {
+        $('#input10').addClass("present")
+    }
+    if (currentTime < 10) {
+        $('#input10').addClass("future")
+    }
+    if (currentTime > 11) {
+        $('#input11').addClass("past")
+    }
+    if (currentTime == 11) {
+        $('#input11').addClass("present")
+    }
+    if (currentTime < 11) {
+        $('#input11').addClass("future")
+    }
+    if (currentTime > 12) {
+        $('#input12').addClass("past")
+    }
+    if (currentTime == 12) {
+        $('#input12').addClass("present")
+    }
+    if (currentTime < 12) {
+        $('#input12').addClass("future")
+    }
+    if (currentTime > 13) {
+        $('#input1').addClass("past")
+    }
+    if (currentTime == 13) {
+        $('#input1').addClass("present")
+    }
+    if (currentTime < 13) {
+        $('#input1').addClass("future")
+    }
+    if (currentTime > 14) {
+        $('#input2').addClass("past")
+    }
+    if (currentTime == 14) {
+        $('#input2').addClass("present")
+    }
+    if (currentTime < 14) {
+        $('#input2').addClass("future")
+    }
+    if (currentTime > 15) {
+        $('#input3').addClass("past")
+    }
+    if (currentTime == 15) {
+        $('#input3').addClass("present")
+    }
+    if (currentTime < 15) {
+        $('#input3').addClass("future")
+    }
+    if (currentTime > 16) {
+        $('#input4').addClass("past")
+    }
+    if (currentTime == 16) {
+        $('#input4').addClass("present")
+    }
+    if (currentTime < 16) {
+        $('#input4').addClass("future")
+    }
+    if (currentTime > 17) {
+        $('#input5').addClass("past")
+    }
+    if (currentTime == 17) {
+        $('#input5').addClass("present")
+    }
+    if (currentTime < 17) {
+        $('#input5').addClass("future")
+    }
+  };
+
+checkTime();
